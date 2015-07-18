34a4cf9567e06e8e070aa8f8dd5dd495a87ebe81
rodolfovalentim <rodolfovieiravalentim@gmail.com>
2015-07-18 02:57:11 +0200

    Main renamed

--- src/enrico/Rodolfo.java
+++ src/enrico/Rodolfo.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import enricoDAO.AnimeDAO;
 import fansubs.AnimaKai;
 
-public class Main {
+public class Rodolfo {
 
 	public static void main(String[] args) throws InterruptedException,
 			IOException {
